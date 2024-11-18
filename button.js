ce136015c585104080eb3fed72007307d4435426
natalie king <108791553+nataliemakhijani@users.noreply.github.com>
2024-11-18 15:20:07 -0500

    hell yeah

--- button.js
+++ button.js
@@ -6,5 +6,5 @@ let buttonState = false
 backgroundButton.addEventListener("click", () => {
     document.body.classList.toggle("background-button-active")
     buttonState = !buttonState
-    if (!buttonState) alert("That fucking sucked")
+    if (!buttonState) alert("That fucking sucked. Good choice")
 })
